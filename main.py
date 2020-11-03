7b2f2ba81be94213359f49536764e4e660be54ce
Gabriele Belvederesi <gabriele.belvederesi.20@ucl.ac.uk>
2020-11-03 19:51:45 +0100

    changed to exercise 6 code

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 def print_string():
-	yourname = input('Please input your full name: ')
-	print(len(yourname))
+	print("code started.")
 
 if __name__=='__main__':
 	print_string()
+
+c=True
+while c:
+    n = int(input('Please enter an integer: '))
+    if n < 1 or n > 7:
+        print("not a day!")
+        c=False
+    elif n == 1:
+        print('Monday')
+    elif n == 2:
+        print('Tuesday')
+    elif n == 3:
+        print('Wednesday')
+    elif n == 4:
+        print('Thursday')
+    elif n == 5:
+        print('Friday')
+    elif n == 6:
+        print('Saturday')
+    else:
+        print('Sunday')
