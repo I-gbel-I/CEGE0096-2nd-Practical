8b06b9bb556abc7732fc817ebe506d98113ff0f0
Gabriele Belvederesi <gabriele.belvederesi.20@ucl.ac.uk>
2020-11-03 20:14:42 +0100

    Ex 13 - code updated with the Fibonacci series

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ if __name__ == '__main__':
 fibo_n = int(input("enter a number for the fibonacci series:\n"))
 
 n1, n2 = 0, 1
-count = 0
+t = 0
 
-print("Fibonacci sequence:")
-while count < fibo_n:
+print("The Fibonacci series is:")
+while t < fibo_n:
     print(n1)
     nth = n1 + n2
     # update values
     n1 = n2
     n2 = nth
-    count += 1
+    t += 1
